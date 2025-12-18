2c4f6d18e841ddb0e89a1b2db9d14d54c30e9fa6
Kakashi7860 <sohelshakh91@gmail.com>
2025-12-18 18:24:12 +0530

    first p

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = "Akash"
+name = "Sahil"
+
+const DOB = 1998
+const num = "12"
+console.log(num+DOB)
+
+console.log(name+DOB)
+
+
+
+
+
+console.log(name)
+console.log(DOB)
+
+for(let i = 1;i<=10;i++){
+    console.log("Heelo")
+}
